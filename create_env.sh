7c66bb67d1633b19eb388a9a751444f923ea1438
Daniel Rose <drosea@amazon.com>
2020-12-17 09:08:49 +0000

    [DR]udates to script

--- create_env.sh
+++ create_env.sh
@@ -3,6 +3,12 @@ TFVER="0.13.5"
 KUBCTLVER="https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl"
 IAMAUTHVER="https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/aws-iam-authenticator"
 
+TOOLS=(
+    "nano"
+    "bind-utils"
+    "bash-completion"
+)
+
 ## PRE CONFIG
 mkdir -p $HOME/bin
 mkdir -p $HOME/tmp
@@ -31,9 +37,14 @@ if [ ! -f $HOME/bin/aws-iam-authenticator ]; then
 fi
 
 ### Tools
-sudo yum install -y -q nano
-sudo yum install -y -q bind-utils
-sudo yum install -y -q bash-completion
+for tool in ${TOOLS[@]};
+    if command -v apt-get >/dev/null; then
+        sudo apt-get -y install $tool
+    elif command -v yum >/dev/null; then
+        sudo yum install -y $tool 
+    else
+        echo "I have no Idea what im doing here"
+fi
 
 ######## Copy files
 sudo cp ./config/.git-prompt.sh ~/
